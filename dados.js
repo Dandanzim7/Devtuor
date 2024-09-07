92ece7799f2f5d15d68ef64434389f25939e35dc
Dandanzim7 <dani453.2020@gmail.com>
2024-09-07 19:47:10 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Html",
+        descricao: "HTML, sigla para HyperText Markup Language (Linguagem de Marcação de Hipertexto), é a linguagem fundamental utilizada para criar a estrutura das páginas da web. É como se fosse o esqueleto de um site, definindo a organização dos elementos visuais como textos, imagens, vídeos e links.",
+        link: "https://pt.wikipedia.org/wiki/HTML",
+        tags: "semantica hipertexto marcação "
+    },
+    {
+        titulo: "Css",
+        descricao: "CSS, sigla para Cascading Style Sheets (Folhas de Estilo em Cascata), é uma linguagem de estilo utilizada para formatar a apresentação de documentos. Em outras palavras, o CSS define como os elementos de uma página web serão exibidos na tela, controlando cores, fontes, layouts e muito mais..",
+        link: "https://pt.wikipedia.org/wiki/Cascading_Style_Sheets",
+        tags: "estilos estilo visual aparencia color text-size"
+    },
+    {
+        titulo: "JavaScript",
+        descricao: "JavaScript é uma linguagem de programação indispensável para criar páginas da web dinâmicas e interativas. Ela é como a alma de um site, dando vida aos elementos estáticos de outras linguagens.",
+        link: "https://pt.wikipedia.org/wiki/JavaScript",
+        tags: "menus menu interativo interatividad "
+    }
+];
